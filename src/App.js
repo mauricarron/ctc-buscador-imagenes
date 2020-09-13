e2715d8f0d21c98ba5e394323f80957e02093d4f
mdCarron <59117899+mdCarron@users.noreply.github.com>
2020-09-13 20:55:35 -0300

    Completa 7. Primeros pasos con el paginador

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import ListaImagen from "./components/ListaImagenes";
 function App() {
   const [busqueda, guardarBusqueda] = useState("");
   const [imagenes, guardarImagenes] = useState([]);
+  const [paginaactual, guardarPaginaActual] = useState(1);
+  const [totalpaginas, guardarTotalPaginas] = useState(1);
 
   useEffect(() => {
     if (busqueda === "") return;
     const consultarApi = async () => {
       const API_KEY = "18295173-25b8442835d7e79f6753c167e";
-      const imagenesPorPagina = "30";
+      const imagenesPorPagina = 30;
       const url =
         `https://pixabay.com/api/` +
         `?key=${API_KEY}&` +
@@ -19,9 +21,15 @@ function App() {
 
       const respuesta = await axios.get(url);
       guardarImagenes(respuesta.data.hits);
+
+      const calcularTotalPaginas = Math.ceil(
+        respuesta.data.totalHits / imagenesPorPagina
+      );
+      guardarTotalPaginas(calcularTotalPaginas);
     };
     consultarApi();
   }, [busqueda]);
+
   return (
     <div className="container">
       <div className="jumbotron">
